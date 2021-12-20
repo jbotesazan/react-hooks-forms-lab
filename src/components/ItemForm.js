3409821bf23f6a28c483fe1ca41cb8cd0452b320
Jason Botesazan <jbotesazan@gmail.com>
2021-12-20 11:14:28 -0500

    commit tree bd65fe14ad1a6c5b4a725977221b338e5aebf263

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,31 @@
 import React from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onNewItemChange, newItem, newItemCategory, onSetNewItemCategory, items, setItems}) {
+
+// console.log(onItemFormSubmit)
+
+    function onItemFormSubmit (e) {
+    e.preventDefault();
+
+    const freshItem = {
+      id: uuid(), // the `uuid` library can be used to generate a unique id
+      name: newItem,
+      category: newItemCategory,
+    }
+    setItems([...items, freshItem]);
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={onItemFormSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={newItem} onChange={onNewItemChange}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={newItemCategory} onChange={onSetNewItemCategory}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
@@ -24,3 +38,5 @@ function ItemForm(props) {
 }
 
 export default ItemForm;
+
+// Take a look at how to get the new items to show up on the App.
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,47 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, setItems }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchItem, setSearchItem] = useState('');
+  const [newItemCategory, setNewItemCategory] = useState('Produce');
+  const [newItem, setNewItem] = useState('');
+
+
+  function onSetNewItemCategory (e) {
+    setNewItemCategory(e.target.value);
+  }
+
+  function onNewItemChange (e) {
+    setNewItem(e.target.value);
+  }
+
+  function onSearchChange (e) {
+    setSearchItem(e.target.value);
+  }
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
   const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+    let itemNorm = item.name.toLowerCase();
+    let query = searchItem.toLowerCase();
 
-    return item.category === selectedCategory;
+    if (selectedCategory === "All" && query.length <= 0) return true; // Case 1: we aren't filtering by substring and the category is All
+    else if (selectedCategory === "All" && query.length > 0) {
+      return itemNorm.indexOf(query) !== -1; //Case 2: We are filtering by substring and category is All
+    }
+    else if (query.length > 0){
+        return (item.category === selectedCategory) && (itemNorm.indexOf(query) !== -1) // Case 3: We are filtering by substring and category is something specific
+    }   
+    return item.category === selectedCategory; // Case 4: We are just filtering by category
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onNewItemChange={onNewItemChange} newItem={newItem} newItemCategory={newItemCategory} onSetNewItemCategory={onSetNewItemCategory} items={items} setItems={setItems} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={onSearchChange} searchItem={searchItem}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
